66f14ba13e2336f3f812138f4b6e132696fd3618
Kakashi hatake <105074780+kakashuuu@users.noreply.github.com>
2025-02-14 12:22:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const puppeteer = require('puppeteer');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 async function fetchRandomCardId() {
     const browser = await puppeteer.launch({ headless: true });
